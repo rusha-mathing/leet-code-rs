61190d9fb13b9b09dd49e7838b9cf42edeae9f25
Myp4aJTKa <rushan.davlitshin.0809@gmail.com>
2023-10-26 19:43:15 +0500

    Solution with O(n^2).

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,13 @@ pub struct Solution();
 
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        todo!("WRITE SOLUTION")
+        for (i, x) in nums.iter().enumerate() {
+            for (j, y) in nums.iter().enumerate().skip(i + 1) {
+                if x + y == target {
+                    return vec![i as i32, j as i32];
+                }
+            }
+        }
+        vec![]
     }
 }
