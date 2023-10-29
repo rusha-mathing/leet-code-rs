3fb7d1a1c63cf268ed7f33725a8fc426b270a763
rusha-mathing <rushan.is.into.math@gmail.com>
2023-10-29 09:45:42 +0500

    Simplifying the code, removing repetition.

--- src/lib.rs
+++ src/lib.rs
@@ -5,15 +5,12 @@ pub struct Solution();
 type Node = Option<Box<ListNode>>;
 
 impl Solution {
-    pub fn add_two_numbers(
-        mut l1: Node,
-        mut l2: Node,
-    ) -> Node {
+    pub fn add_two_numbers(mut l1: Node, mut l2: Node) -> Node {
         let mut head = None;
         let mut tail = &mut head;
         let mut remainder = 0;
         loop {
-            match (l1, l2) {
+            let val = match (l1, l2) {
                 (None, None) => {
                     if remainder != 0 {
                         *tail = Some(Box::new(ListNode::new(remainder)));
@@ -21,21 +18,16 @@ impl Solution {
                     return head;
                 }
                 (None, Some(node)) | (Some(node), None) => {
-                    let mut val = node.val + remainder;
-                    remainder = val / 10;
-                    val %= 10;
-                    tail = &mut tail.insert(Box::new(ListNode::new(val))).next;
-                    l1 = node.next;
-                    l2 = None;
+                    (l1, l2) = (node.next, None);
+                    node.val
                 }
                 (Some(left), Some(right)) => {
-                    let mut val = left.val + right.val + remainder;
-                    remainder = val / 10;
-                    val %= 10;
-                    tail = &mut tail.insert(Box::new(ListNode::new(val))).next;
                     (l1, l2) = (left.next, right.next);
+                    left.val + right.val
                 }
-            }
+            } + remainder;
+            remainder = val / 10;
+            tail = &mut tail.insert(Box::new(ListNode::new(val % 10))).next;
         }
     }
 }
