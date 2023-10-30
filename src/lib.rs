0a63636a09c0e0e00e58bf1777bf89a111c4c4ff
rusha-mathing <rushan.is.into.math@gmail.com>
2023-10-30 18:20:54 +0500

    Dumb solution. Doesn't take into account sorted arrays.

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,18 @@ pub struct Solution();
 
 impl Solution {
     pub fn find_median_sorted_arrays(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
-        todo!("WRITE SOLUTION")
+        let nums = Self::merge(nums1, nums2);
+        let mid = nums.len() / 2;
+        if nums.len() % 2 == 1 {
+            nums[mid] as f64
+        } else {
+            (nums[mid - 1] + nums[mid]) as f64 / 2.0
+        }
+    }
+
+    fn merge(mut nums1: Vec<i32>, nums2: Vec<i32>) -> Vec<i32> {
+        nums1.extend(nums2);
+        nums1.sort();
+        nums1
     }
 }
