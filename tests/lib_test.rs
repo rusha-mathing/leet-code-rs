ec4a77537422389878ae161673d97d08ff1535b1
rusha-mathing <rushan.is.into.math@gmail.com>
2023-11-01 17:18:58 +0500

    Change test asserting.

--- tests/lib_test.rs
+++ tests/lib_test.rs
@@ -3,107 +3,107 @@
 mod tests {
     use leet_code_rs::*;
 
-    fn helper(s: &str, expect: &str) {
-        assert_eq!(Solution::longest_palindrome(s.to_owned()), expect);
+    fn helper(s: &str, expect: &[&str]) {
+        assert!(expect.contains(&Solution::longest_palindrome(s.to_owned()).as_str()));
     }
 
     #[test]
     fn test_1() {
-        helper("babad", "bab");
+        helper("babad", &["bab", "aba"]);
     }
     
     #[test]
     fn test_2() {
-        helper("cbbd", "bb");
+        helper("cbbd", &["bb"]);
     }
     
     #[test]
     fn test_3() {
-        helper("a", "a");
+        helper("a", &["a"]);
     }
     
     #[test]
     fn test_4() {
-        helper("ac", "a");
+        helper("ac", &["a", "c"]);
     }
     
     #[test]
     fn test_5() {
-        helper("bb", "bb");
+        helper("bb", &["bb"]);
     }
     
     #[test]
     fn test_6() {
-        helper("abcbe", "bcb");
+        helper("abcbe", &["bcb"]);
     }
     
     #[test]
     fn test_7() {
-        helper("abcde", "a");
+        helper("abcde", &["a", "b", "c", "d", "e"]);
     }
     
     #[test]
     fn test_8() {
-        helper("babadbbabb", "bbabb");
+        helper("babadbbabb", &["bbabb"]);
     }
     
     #[test]
     fn test_9() {
-        helper("aaaa", "aaaa");
+        helper("aaaa", &["aaaa"]);
     }
     
     #[test]
     fn test_10() {
-        helper("aaabaaaa", "aaabaaa");
+        helper("aaabaaaa", &["aaabaaa"]);
     }
     
     #[test]
     fn test_11() {
-        helper("racecar", "racecar");
+        helper("racecar", &["racecar"]);
     }
     
     #[test]
     fn test_12() {
-        helper("level", "level");
+        helper("level", &["level"]);
     }
     
     #[test]
     fn test_13() {
-        helper("madam", "madam");
+        helper("madam", &["madam"]);
     }
     
     #[test]
     fn test_14() {
-        helper("deified", "deified");
+        helper("deified", &["deified"]);
     }
     
     #[test]
     fn test_15() {
-        helper("helloolleh", "helloolleh");
+        helper("helloolleh", &["helloolleh"]);
     }
     
     #[test]
     fn test_16() {
-        helper("aaaaab", "aaaaa");
+        helper("aaaaab", &["aaaaa"]);
     }
     
     #[test]
     fn test_17() {
-        helper("abcdeedcba", "abcdeedcba");
+        helper("abcdeedcba", &["abcdeedcba"]);
     }
     
     #[test]
     fn test_18() {
-        helper("abcdedcba", "abcdedcba");
+        helper("abcdedcba", &["abcdedcba"]);
     }
     
     #[test]
     fn test_19() {
-       helper ("abccba", "abccba");
+       helper ("abccba", &["abccba"]);
     }
     
     #[test]
     fn test_20() {
-       helper ("abcabcabc", "a");
+       helper ("abcabcabc", &["a", "b", "c"]);
     }
 }
