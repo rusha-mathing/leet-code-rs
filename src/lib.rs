cf633d9f0113e2f489ab665a7075e061586bb9e5
rusha-mathing <rushan.is.into.math@gmail.com>
2023-11-01 15:54:26 +0500

    Add tests from ChatGPT. First solution(brute force)

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,21 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub struct Solution();
+
+impl Solution {
+    pub fn longest_palindrome(s: String) -> String {
+        (0..s.len())
+            .map(|i| {
+                (i..s.len())
+                    .map(|j| &s[i..=j])
+                    .filter(|&arg| Self::is_palindrome(arg))
+                    .max_by_key(|x| x.len())
+                    .unwrap_or_default()
+            })
+            .max_by_key(|x| x.len())
+            .unwrap_or_default()
+            .to_owned()
+    }
+
+    fn is_palindrome(s: &str) -> bool {
+        s.chars().eq(s.chars().rev())
+    }
 }
--- tests/lib_test.rs
+++ tests/lib_test.rs
@@ -3,9 +3,107 @@
 mod tests {
     use leet_code_rs::*;
 
+    fn helper(s: &str, expect: &str) {
+        assert_eq!(Solution::longest_palindrome(s.to_owned()), expect);
+    }
+
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_1() {
+        helper("babad", "bab");
+    }
+    
+    #[test]
+    fn test_2() {
+        helper("cbbd", "bb");
+    }
+    
+    #[test]
+    fn test_3() {
+        helper("a", "a");
+    }
+    
+    #[test]
+    fn test_4() {
+        helper("ac", "a");
+    }
+    
+    #[test]
+    fn test_5() {
+        helper("bb", "bb");
+    }
+    
+    #[test]
+    fn test_6() {
+        helper("abcbe", "bcb");
+    }
+    
+    #[test]
+    fn test_7() {
+        helper("abcde", "a");
+    }
+    
+    #[test]
+    fn test_8() {
+        helper("babadbbabb", "bbabb");
+    }
+    
+    #[test]
+    fn test_9() {
+        helper("aaaa", "aaaa");
+    }
+    
+    #[test]
+    fn test_10() {
+        helper("aaabaaaa", "aaabaaa");
+    }
+    
+    #[test]
+    fn test_11() {
+        helper("racecar", "racecar");
+    }
+    
+    #[test]
+    fn test_12() {
+        helper("level", "level");
+    }
+    
+    #[test]
+    fn test_13() {
+        helper("madam", "madam");
+    }
+    
+    #[test]
+    fn test_14() {
+        helper("deified", "deified");
+    }
+    
+    #[test]
+    fn test_15() {
+        helper("helloolleh", "helloolleh");
+    }
+    
+    #[test]
+    fn test_16() {
+        helper("aaaaab", "aaaaa");
+    }
+    
+    #[test]
+    fn test_17() {
+        helper("abcdeedcba", "abcdeedcba");
+    }
+    
+    #[test]
+    fn test_18() {
+        helper("abcdedcba", "abcdedcba");
+    }
+    
+    #[test]
+    fn test_19() {
+       helper ("abccba", "abccba");
+    }
+    
+    #[test]
+    fn test_20() {
+       helper ("abcabcabc", "a");
     }
 }
