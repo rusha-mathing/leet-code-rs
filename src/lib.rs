68e2cfd6c7f024e77610ba5e141de63ae20dfc9b
rusha-mathing <rushan.is.into.math@gmail.com>
2023-11-08 16:53:54 +0500

    Add tests from ChatGPT. Solve problem.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,19 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub struct Solution;
+
+impl Solution {
+    pub fn reverse(x: i32) -> i32 {
+        Self::reverse_result(x).unwrap_or_default()
+    }
+
+    fn reverse_result(x: i32) -> Option<i32> {
+        let (mut current, mut total): (i32, i32) = (x.checked_abs()?, 0);
+        while current > 0 {
+            total = total.checked_mul(10)?.checked_add(current % 10)?;
+            current /= 10;
+        }
+        if x < 0 {
+            total = -total;
+        }
+        Some(total)
+    }
 }
--- tests/lib_test.rs
+++ tests/lib_test.rs
@@ -4,8 +4,127 @@ mod tests {
     use leet_code_rs::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_1() {
+        assert_eq!(Solution::reverse(123), 321);
+    }
+    
+    #[test]
+    fn test_2() {
+        assert_eq!(Solution::reverse(-321), -123);
+    }
+    
+    #[test]
+    fn test_3() {
+        assert_eq!(Solution::reverse(120), 21);
+    }
+    
+    #[test]
+    fn test_4() {
+        assert_eq!(Solution::reverse(0), 0);
+    }
+    
+    #[test]
+    fn test_5() {
+        assert_eq!(Solution::reverse(5), 5);
+    }
+    
+    #[test]
+    fn test_6() {
+        assert_eq!(Solution::reverse(1534236469), 0);
+    }
+    
+    #[test]
+    fn test_7() {
+        assert_eq!(Solution::reverse(-1463847414), 0);
+    }
+    
+    #[test]
+    fn test_8() {
+        assert_eq!(Solution::reverse(-2147483648), 0);
+    }
+    
+    #[test]
+    fn test_9() {
+        assert_eq!(Solution::reverse(1563847412), 0);
+    }
+    
+    #[test]
+    fn test_10() {
+        assert_eq!(Solution::reverse(-1563847412), 0);
+    }
+    
+    #[test]
+    fn test_11() {
+        assert_eq!(Solution::reverse(2147483647), 0);
+    }
+    
+    #[test]
+    fn test_12() {
+        assert_eq!(Solution::reverse(-1563847427), 0);
+    }
+    
+    #[test]
+    fn test_13() {
+        assert_eq!(Solution::reverse(1463847412), 2147483641);
+    }
+    
+    #[test]
+    fn test_14() {
+        assert_eq!(Solution::reverse(1463847413), 0);
+    }
+    
+    #[test]
+    fn test_15() {
+        assert_eq!(Solution::reverse(1463847421), 1247483641);
+    }
+    
+    #[test]
+    fn test_16() {
+        assert_eq!(Solution::reverse(1000000003), 0);
+    }
+    
+    #[test]
+    fn test_17() {
+        assert_eq!(Solution::reverse(10), 1);
+    }
+    
+    #[test]
+    fn test_18() {
+        assert_eq!(Solution::reverse(-10), -1);
+    }
+    
+    #[test]
+    fn test_19() {
+        assert_eq!(Solution::reverse(1000000000), 1);
+    }
+    
+    #[test]
+    fn test_20() {
+        assert_eq!(Solution::reverse(-1000000003), 0);
+    }
+    
+    #[test]
+    fn test_21() {
+        assert_eq!(Solution::reverse(1534236469), 0);
+    }
+    
+    #[test]
+    fn test_22() {
+        assert_eq!(Solution::reverse(-1534236469), 0);
+    }
+    
+    #[test]
+    fn test_23() {
+        assert_eq!(Solution::reverse(1563847412), 0);
+    }
+    
+    #[test]
+    fn test_24() {
+        assert_eq!(Solution::reverse(-1563847412), 0);
+    }
+    
+    #[test]
+    fn test_25() {
+        assert_eq!(Solution::reverse(2147483646), 0);
     }
 }
