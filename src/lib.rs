687cd7ea5891839d61ca96318ad4fc05f6aac567
rusha-mathing <rushan.is.into.math@gmail.com>
2023-11-09 19:51:45 +0500

    Add 30 tests.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub struct Solution;
+
+impl Solution {
+    pub fn is_match(s: String, p: String) -> bool {
+        Self::is_match_chars(s.chars(), p.chars())
+    }
+
+    pub fn is_match_chars(s: impl Iterator<Item = char>, p: impl Iterator<Item = char>) -> bool {
+        todo!()
+    }
 }
--- tests/lib_test.rs
+++ tests/lib_test.rs
@@ -1,11 +1,154 @@
-
 #[cfg(test)]
 mod tests {
-    use leet_code_rs::*;
+    use leet_code_rs::Solution;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_1() {
+        assert_eq!(Solution::is_match(String::from("aa"), String::from("a")), false);
+    }
+
+    #[test]
+    fn test_2() {
+        assert_eq!(Solution::is_match(String::from("aa"), String::from("a*")), true);
+    }
+
+    #[test]
+    fn test_3() {
+        assert_eq!(Solution::is_match(String::from("ab"), String::from(".*")), true);
+    }
+
+    #[test]
+    fn test_4() {
+        assert_eq!(Solution::is_match(String::from("aab"), String::from("c*a*b")), true);
+    }
+
+    #[test]
+    fn test_5() {
+        assert_eq!(Solution::is_match(String::from("mississippi"), String::from("mis*is*p*.")), false);
+    }
+
+    #[test]
+    fn test_6() {
+        assert_eq!(Solution::is_match(String::from("a"), String::from("a")), true);
+    }
+
+    #[test]
+    fn test_7() {
+        assert_eq!(Solution::is_match(String::from("xy"), String::from(".*")), true);
+    }
+
+    #[test]
+    fn test_8() {
+        assert_eq!(Solution::is_match(String::from(""), String::from("a*")), true);
+    }
+
+    #[test]
+    fn test_9() {
+        assert_eq!(Solution::is_match(String::from(""), String::from("")), true);
+    }
+
+    #[test]
+    fn test_10() {
+        assert_eq!(Solution::is_match(String::from("a"), String::from("")), false);
+    }
+
+    #[test]
+    fn test_11() {
+        assert_eq!(Solution::is_match(String::from("aaa"), String::from("a*a")), true);
+    }
+
+    #[test]
+    fn test_12() {
+        assert_eq!(Solution::is_match(String::from("aaa"), String::from("ab*a*c*a")), true);
+    }
+
+    #[test]
+    fn test_13() {
+        assert_eq!(Solution::is_match(String::from("aaa"), String::from(".*")), true);
+    }
+
+    #[test]
+    fn test_14() {
+        assert_eq!(Solution::is_match(String::from("aaa"), String::from("a.*")), true);
+    }
+
+    #[test]
+    fn test_15() {
+        assert_eq!(Solution::is_match(String::from("aaa"), String::from("a.*a")), true);
+    }
+
+    #[test]
+    fn test_16() {
+        assert_eq!(Solution::is_match(String::from("aaa"), String::from("a.*aa")), true);
+    }
+
+    #[test]
+    fn test_17() {
+        assert_eq!(Solution::is_match(String::from("aaa"), String::from("a.*aaa")), false);
+    }
+
+    #[test]
+    fn test_18() {
+        assert_eq!(Solution::is_match(String::from("aaa"), String::from("a.*aaaa")), false);
+    }
+
+    #[test]
+    fn test_19() {
+        assert_eq!(Solution::is_match(String::from("aaa"), String::from("a.*aaaaa")), false);
+    }
+
+    #[test]
+    fn test_20() {
+        assert_eq!(Solution::is_match(String::from("aaa"), String::from("a.*aaaaaa")), false);
+    }
+
+    #[test]
+    fn test_21() {
+        assert_eq!(Solution::is_match(String::from("aaa"), String::from("a.*aaaaaaa")), false);
+    }
+
+    #[test]
+    fn test_22() {
+        assert_eq!(Solution::is_match(String::from("aaa"), String::from("a.*aaaaaaaa")), false);
+    }
+
+    #[test]
+    fn test_23() {
+        assert_eq!(Solution::is_match(String::from("aaa"), String::from("a.*aaaaaaaaa")), false);
+    }
+
+    #[test]
+    fn test_24() {
+        assert_eq!(Solution::is_match(String::from("aaa"), String::from("a.*aaaaaaaaaa")), false);
+    }
+
+    #[test]
+    fn test_25() {
+        assert_eq!(Solution::is_match(String::from("aaa"), String::from("a.*aaaaaaaaaaa")), false);
+    }
+
+    #[test]
+    fn test_26() {
+        assert_eq!(Solution::is_match(String::from("aaa"), String::from("a.*aaaaaaaaaaaa")), false);
+    }
+
+    #[test]
+    fn test_27() {
+        assert_eq!(Solution::is_match(String::from("aaa"), String::from("a.*aaaaaaaaaaaaa")), false);
+    }
+
+    #[test]
+    fn test_28() {
+        assert_eq!(Solution::is_match(String::from("aaa"), String::from("a.*aaaaaaaaaaaaaa")), false);
+    }
+
+    #[test]
+    fn test_29() {
+        assert_eq!(Solution::is_match(String::from("aaa"), String::from("a.*aaaaaaaaaaaaaaa")), false);
+    }
+
+    #[test]
+    fn test_30() {
+        assert_eq!(Solution::is_match(String::from("aaa"), String::from("a.*aaaaaaaaaaaaaaaa")), false);
     }
 }
