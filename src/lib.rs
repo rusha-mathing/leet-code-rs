43517defd3f7e04da909953e875d4351af571764
rusha-mathing <rushan.is.into.math@gmail.com>
2023-11-09 19:05:25 +0500

    Add tests from ChatGPT. First solution.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,17 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub struct Solution;
+
+impl Solution {
+    pub fn is_palindrome(mut x: i32) -> bool {
+        if x < 0 {
+            return false;
+        } else if x == 0 {
+            return true;
+        } 
+        let mut digits = Vec::with_capacity(x.checked_ilog10().unwrap() as usize);
+        while x > 0 {
+            digits.push(x % 10);
+            x /= 10;
+        }
+        digits.iter().eq(digits.iter().rev())
+    }
 }
--- tests/lib_test.rs
+++ tests/lib_test.rs
@@ -1,11 +1,104 @@
-
 #[cfg(test)]
 mod tests {
     use leet_code_rs::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_1() {
+        assert_eq!(Solution::is_palindrome(121), true);
+    }
+
+    #[test]
+    fn test_2() {
+        assert_eq!(Solution::is_palindrome(-121), false);
+    }
+
+    #[test]
+    fn test_3() {
+        assert_eq!(Solution::is_palindrome(10), false);
+    }
+
+    #[test]
+    fn test_4() {
+        assert_eq!(Solution::is_palindrome(0), true);
+    }
+
+    #[test]
+    fn test_5() {
+        assert_eq!(Solution::is_palindrome(12321), true);
+    }
+
+    #[test]
+    fn test_6() {
+        assert_eq!(Solution::is_palindrome(123456789), false);
+    }
+
+    #[test]
+    fn test_7() {
+        assert_eq!(Solution::is_palindrome(111), true);
+    }
+
+    #[test]
+    fn test_8() {
+        assert_eq!(Solution::is_palindrome(987654321), false);
+    }
+
+    #[test]
+    fn test_9() {
+        assert_eq!(Solution::is_palindrome(1001), true);
+    }
+
+    #[test]
+    fn test_10() {
+        assert_eq!(Solution::is_palindrome(1234321), true);
+    }
+
+    #[test]
+    fn test_11() {
+        assert_eq!(Solution::is_palindrome(123454321), true);
+    }
+
+    #[test]
+    fn test_12() {
+        assert_eq!(Solution::is_palindrome(135797531), true);
+    }
+
+    #[test]
+    fn test_13() {
+        assert_eq!(Solution::is_palindrome(135798531), false);
+    }
+
+    #[test]
+    fn test_14() {
+        assert_eq!(Solution::is_palindrome(1000021), false);
+    }
+
+    #[test]
+    fn test_15() {
+        assert_eq!(Solution::is_palindrome(123210), false);
+    }
+
+    #[test]
+    fn test_16() {
+        assert_eq!(Solution::is_palindrome(987654321), false);
+    }
+
+    #[test]
+    fn test_17() {
+        assert_eq!(Solution::is_palindrome(121212121), true);
+    }
+
+    #[test]
+    fn test_18() {
+        assert_eq!(Solution::is_palindrome(9876789), true);
+    }
+
+    #[test]
+    fn test_19() {
+        assert_eq!(Solution::is_palindrome(1234543210), false);
+    }
+
+    #[test]
+    fn test_20() {
+        assert_eq!(Solution::is_palindrome(1001001), true);
     }
 }
