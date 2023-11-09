cbe4983f595813cc204bb0fd9638b230fbfb0c7c
rusha-mathing <rushan.is.into.math@gmail.com>
2023-11-09 19:13:13 +0500

    Remove pointless vector allocating.

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,15 @@
 pub struct Solution;
 
 impl Solution {
-    pub fn is_palindrome(mut x: i32) -> bool {
-        if x < 0 {
-            return false;
-        } else if x == 0 {
-            return true;
-        } 
-        let mut digits = Vec::with_capacity(x.checked_ilog10().unwrap() as usize);
-        while x > 0 {
-            digits.push(x % 10);
-            x /= 10;
+    pub fn is_palindrome(x: i32) -> bool {
+        if x <= 0 {
+            return x == 0;
         }
-        digits.iter().eq(digits.iter().rev())
+        let (mut rev, mut temp): (i32, i32) = (0, x);
+        while temp > 0 {
+            rev = rev.saturating_mul(10).saturating_add(temp % 10);
+            temp /= 10;
+        }
+        rev == x
     }
 }
