ee540dd011475118d2872c0e87bbe46820f4f35b
rusha-mathing <rushan.is.into.math@gmail.com>
2023-12-11 21:19:41 +0500

    Add solution with hash map (memorization).

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,26 @@
+use std::collections::{HashMap, HashSet};
+
 pub struct Solution;
 
 impl Solution {
     pub fn four_sum(nums: Vec<i32>, target: i32) -> Vec<Vec<i32>> {
-       todo!()
+        let mut result = HashSet::new();
+        let mut cash_memory: HashMap<i32, usize> = HashMap::new();
+        for (i, x) in nums.iter().enumerate() {
+            for (j, y) in nums.iter().enumerate().skip(i + 1) {
+                for z in nums.iter().skip(j + 1) {
+                    let remaind = target - (x + y + z);
+                    if cash_memory.contains_key(&remaind) {
+                        result.insert({
+                            let mut quadruplets = vec![*x, *y, *z, remaind];
+                            quadruplets.sort();
+                            quadruplets
+                        });
+                    }
+                }
+            }
+            cash_memory.insert(*x, i);
+        }
+        result.into_iter().collect()
     }
-
 }
