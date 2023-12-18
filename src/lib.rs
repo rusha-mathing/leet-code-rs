10ed2bd1147f6e91f7530287bc2fbe8907ff7299
rusha-mathing <rushan.is.into.math@gmail.com>
2023-12-18 11:24:33 +0500

    Add first solution with self-writing tests.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,33 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub struct Solution;
+
+const OPEN_BRACKETS: &str = "([{";
+const CLOSE_BRACKETS: &str = ")]}";
+
+impl Solution {
+    pub fn is_valid(s: String) -> bool {
+        if s.len() % 2 == 1 {
+            return false;
+        }
+        let mut stack = Vec::new();
+
+        for ch in s.chars() {
+            if OPEN_BRACKETS.contains(ch) {
+                stack.push(ch);
+            } else if CLOSE_BRACKETS.contains(ch) {
+                let open_bracket = match ch {
+                    ')' => '(',
+                    ']' => '[',
+                    '}' => '{',
+                    unknown => unimplemented!("Unknown close bracket '{unknown}'"),
+                };
+                if stack.pop() != Some(open_bracket) {
+                    return false;
+                }
+            } else {
+                panic!("s must consists of parentheses only '()[]{{}}'");
+            }
+        }
+
+        stack.is_empty()
+    }
 }
--- tests/lib_test.rs
+++ tests/lib_test.rs
@@ -3,9 +3,63 @@
 mod tests {
     use leet_code_rs::*;
 
+    fn helper(s: &str, val: bool) {
+        assert_eq!(Solution::is_valid(s.into()), val)
+    } 
+
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_1() {
+        helper("()", true)
     }
+
+    #[test]
+    fn test_2() {
+        helper("()[]{}", true)
+    }
+
+    #[test]
+    fn test_3() {
+        helper("(]", false)
+    }
+
+    #[test]
+    fn test_4() {
+        helper("((", false)   
+    }
+
+    #[test]
+    fn test_5() {
+        helper("()()()", true);
+    }
+
+    #[test]
+    fn test_6() {
+        helper("((((((()))))))", true)
+    }
+
+    #[test]
+    fn test_7() {
+        helper("([{[{[((({})))]}]}])", true)
+    }
+
+    #[test]
+    fn test_8() {
+        helper("(()())[(){}]", true)
+    }
+
+    #[test]
+    fn test_9() {
+        helper("([)]", false);
+    }
+
+    #[test]
+    fn test_10() {
+        helper("([])", true)
+    }
+
+    #[test]
+    fn test_11() {
+        helper("(()", false);
+    }
+
 }
