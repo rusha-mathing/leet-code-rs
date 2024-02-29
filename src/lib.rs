e89e1e7c824e989af3ca51881d768e17e1be1742
rusha-mathing <rushan.is.into.math@gmail.com>
2024-02-29 16:18:04 +0500

    Add tests and first solution with recursion (backtracking).

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,24 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub struct Solution;
+
+impl Solution {
+    pub fn generate_parenthesis(n: i32) -> Vec<String> {
+        let mut result = Vec::new();
+        recursion(n, n, String::new(), &mut result);
+        return result;
+
+        fn recursion(open: i32, close: i32, mut current: String, result: &mut Vec<String>) {
+            if open < 0 || close < 0 || open > close {
+                return;
+            }
+            if (open, close) == (0, 0) {
+                result.push(current);
+                return;
+            } 
+            let mut clone = current.clone();
+            clone.push(')');
+            current.push('(');
+            recursion(open - 1, close, current, result);
+            recursion(open, close - 1, clone, result)
+        }
+    }
 }
--- tests/lib_test.rs
+++ tests/lib_test.rs
@@ -1,11 +1,37 @@
-
 #[cfg(test)]
 mod tests {
     use leet_code_rs::*;
 
+    fn helper(n: i32, except: &mut [&str]) {
+        let mut result = Solution::generate_parenthesis(n);
+        result.sort();
+        except.sort();
+        assert_eq!(Solution::generate_parenthesis(n), except)
+    }
+
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_1() {
+        helper(1, &mut ["()"])
+    }
+
+    #[test]
+    fn test_2() {
+        helper(2, &mut ["(())", "()()"])
+    }
+
+    #[test]
+    fn test_3() {
+        helper(3, &mut ["((()))", "(()())", "(())()", "()(())", "()()()"])
+    }
+
+    #[test]
+    fn test_4() {
+        helper(
+            4,
+            &mut [
+                "(((())))", "((()()))", "((())())", "((()))()", "(()(()))", "(()()())", "(()())()",
+                "(())(())", "(())()()", "()((()))", "()(()())", "()(())()", "()()(())", "()()()()",
+            ],
+        )
     }
 }
